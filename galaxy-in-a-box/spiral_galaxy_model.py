f19acc7b0c6b0e47bc5d697dd87e6df697568c21
Markus Thor Rasmussen <str224@ku.dk>
2020-11-04 15:49:54 +0100

    Revert "Optimization of the big loop. It should run MUCH faster now!"
    This reverts commit 816793d893cbd50a9d040bb34f7fb100f6cc55e2.

--- galaxy-in-a-box/spiral_galaxy_model.py
+++ galaxy-in-a-box/spiral_galaxy_model.py
@@ -6,7 +6,7 @@ import math
 import os
 from os import listdir
 from os.path import isfile, join
-import sys
+import sys	
 from sympy.solvers import nsolve
 from sympy import Symbol, exp
 from astropy.utils.data import get_pkg_data_filename
@@ -136,11 +136,14 @@ for n in tff:
 			for i in Mcm:
 				f = open("cluster_setup.dat")
 				fout = open("cluster_setup_change.dat", "wt")
-				for line in f:
+				for line in f: 
 					fout.write(line.replace('10000', str(i)))
-					fout.write(line.replace('0.03', str(s)))
-					fout.write(line.replace('0.0',str(j)))
-					fout.write(line.replace('1.0',str(n)))
+					for line in f:
+						fout.write(line.replace('0.03', str(s)))
+						for line in f:
+							fout.write(line.replace('0.0',str(j)))
+							for line in f:
+								fout.write(line.replace('1.0',str(n)))
 				f.close()
 				fout.close()
 				newname="distributions_Mcm="+str(i)+"_tff="+str(n)+"_imf="+str(j)+"_SFE="+str(s)+".dat"
@@ -149,13 +152,13 @@ for n in tff:
 				os.rename("distribution.dat",newname)
 				g=open("image_setup.dat")
 				gout = open("image_setup_change.dat", "wt")
-				for line in g:
+				for line in g: 
 					gout.write(line.replace('distribution.dat', newname))
 				g.close()
 				gout.close()
 				template=ce.Mod_Template()
 				template.main()
-				try:
+				try: 
 					os.remove(newname)
 				except OSError:
 					pass
@@ -205,17 +208,17 @@ for i in range(0,len(X)):
         d = comb1[i][0]
         data=np.zeros((3,3))
         data[1,1]=d
-
+    
     x=X[i]*56
     y=Y[i]*56
-
+    
     Galaxyarray[int((x+(dims[0]-len(data))/2)):int((x+(dims[0]+len(data))/2)),int((y+(dims[1]-len(data))/2)):int((y+(dims[1]+len(data))/2))]+=data
 #print(np.amax(x))
 config={}
 f=open('image_setup_change.dat','r')
 for line in f.readlines():
     config[line.split()[0]]=line.split()[1]
-
+    
 # Parameters relating to new image
 dist = float(config['bob']) # distance to cluster in pc
 pixel_size = float(config['psize']) # pixel size in arcsec
